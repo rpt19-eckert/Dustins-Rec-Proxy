a6d2b5e02e38c7a430e8650d53921cd0a3aa27f2
Dustin Ancalade <umbi24@gmail.com>
2020-05-28 21:25:30 -0700

    proxy works locally with my indvidual service

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const fullPath = '/Users/jasonjacob/Desktop/seniorProjects/rpt19-front-end-capstone/jason_FEC_proxy/public/index.html';
 
 
 app.use(bodyParser.json());
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/:id', (req, res) => {
-  res.sendFile(fullPath);
+  res.sendFile(path.join(__dirname, '../public', '/index.html'));
 });
 
 app.listen(port, () => {
